01c9391943dd5ba422987857f658c6abeebd7bef
Andres <andmcvey@gmail.com>
2015-07-11 11:32:51 -0400

    adding cancan gem and abilities

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :auntheticate_user!, except: [:index, :show]
+  load_and_authorize_resource
 
   # GET /courses
   # GET /courses.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :courses
+
+  before_save :default_values
+
+  enum role: [:guest, :school, :admin]
+
+  def default_values
+    self.role ||=0 
+  end
+
 end
